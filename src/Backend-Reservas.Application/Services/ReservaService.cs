dc68d10ae2f33c9e4cb79185553d7470bca58f3e
iran <iran.csousa21@gmail.com>
2026-09-04 13:02:05 -0300

    FEAT: Implementação da regra de conflito de horários

--- src/Backend-Reservas.Application/Services/ReservaService.cs
+++ src/Backend-Reservas.Application/Services/ReservaService.cs
@@ -2,6 +2,7 @@ using Backend_Reservas.Application.DTOs.Reserva;
 using Backend_Reservas.Application.Exceptions;
 using Backend_Reservas.Application.Interfaces;
 using Backend_Reservas.Domain.Entities;
+using Backend_Reservas.Domain.Enums;
 
 namespace Backend_Reservas.Application.Services;
 
@@ -61,6 +62,16 @@ public class ReservaService : IReservaService
         if (sala is null)
             throw new SalaNaoEncontradaException(dto.SalaId);
 
+        var reservas = await _reservaRepository.ObterTodasAsync(dto.SalaId);
+
+        var existeConflito = reservas.Any(reserva =>
+            reserva.Status != StatusReserva.Cancelada &&
+            reserva.Inicio < dto.Fim &&
+            reserva.Fim > dto.Inicio);
+
+        if (existeConflito)
+            throw new ReservaConflitanteException();
+
         var reserva = new Reserva
         {
             SalaId = dto.SalaId,
@@ -96,6 +107,17 @@ public class ReservaService : IReservaService
 
         if (sala is null)
             throw new SalaNaoEncontradaException(dto.SalaId);
+
+        var reservas = await _reservaRepository.ObterTodasAsync(dto.SalaId);
+
+        var existeConflito = reservas.Any(reservaExistente =>
+            reservaExistente.Id != id &&
+            reservaExistente.Status != StatusReserva.Cancelada &&
+            reservaExistente.Inicio < dto.Fim &&
+            reservaExistente.Fim > dto.Inicio);
+
+        if (existeConflito)
+            throw new ReservaConflitanteException();
 
         reserva.SalaId = dto.SalaId;
         reserva.Inicio = dto.Inicio;
