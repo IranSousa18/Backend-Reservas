e6c9ba1bfe9da0944e8a01c7847a6df747edda1b
iran <iran.csousa21@gmail.com>
2026-09-04 12:18:24 -0300

    FEAT: Implementação do ReservaService

---
+++ src/Backend-Reservas.Application/Services/ReservaService.cs
@@ -0,0 +1,100 @@
+using Backend_Reservas.Application.DTOs.Reserva;
+using Backend_Reservas.Application.Interfaces;
+using Backend_Reservas.Domain.Entities;
+
+namespace Backend_Reservas.Application.Services;
+
+public class ReservaService : IReservaService
+{
+    private readonly IReservaRepository _reservaRepository;
+
+    public ReservaService(IReservaRepository reservaRepository)
+    {
+        _reservaRepository = reservaRepository;
+    }
+
+    public async Task<IEnumerable<ReservaDto>> ObterTodasAsync(int? salaId = null)
+    {
+        var reservas = await _reservaRepository.ObterTodasAsync(salaId);
+
+        return reservas.Select(reserva => new ReservaDto
+        {
+            Id = reserva.Id,
+            SalaId = reserva.SalaId,
+            Inicio = reserva.Inicio,
+            Fim = reserva.Fim,
+            Responsavel = reserva.Responsavel,
+            Status = reserva.Status
+        });
+    }
+
+    public async Task<ReservaDto?> ObterPorIdAsync(int id)
+    {
+        var reserva = await _reservaRepository.ObterPorIdAsync(id);
+
+        if (reserva is null)
+            return null;
+
+        return new ReservaDto
+        {
+            Id = reserva.Id,
+            SalaId = reserva.SalaId,
+            Inicio = reserva.Inicio,
+            Fim = reserva.Fim,
+            Responsavel = reserva.Responsavel,
+            Status = reserva.Status
+        };
+    }
+
+    public async Task<ReservaDto> CriarAsync(CriarReservaDto dto)
+    {
+        var reserva = new Reserva
+        {
+            SalaId = dto.SalaId,
+            Inicio = dto.Inicio,
+            Fim = dto.Fim,
+            Responsavel = dto.Responsavel
+        };
+
+        await _reservaRepository.AdicionarAsync(reserva);
+
+        return new ReservaDto
+        {
+            Id = reserva.Id,
+            SalaId = reserva.SalaId,
+            Inicio = reserva.Inicio,
+            Fim = reserva.Fim,
+            Responsavel = reserva.Responsavel,
+            Status = reserva.Status
+        };
+    }
+
+    public async Task<bool> AtualizarAsync(int id, AtualizarReservaDto dto)
+    {
+        var reserva = await _reservaRepository.ObterPorIdAsync(id);
+
+        if (reserva is null)
+            return false;
+
+        reserva.SalaId = dto.SalaId;
+        reserva.Inicio = dto.Inicio;
+        reserva.Fim = dto.Fim;
+        reserva.Responsavel = dto.Responsavel;
+
+        await _reservaRepository.AtualizarAsync(reserva);
+
+        return true;
+    }
+
+    public async Task<bool> ExcluirAsync(int id)
+    {
+        var reserva = await _reservaRepository.ObterPorIdAsync(id);
+
+        if (reserva is null)
+            return false;
+
+        await _reservaRepository.ExcluirAsync(reserva);
+
+        return true;
+    }
+}
