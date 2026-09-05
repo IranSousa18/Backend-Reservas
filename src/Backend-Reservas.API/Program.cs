873294a647fd59d6e552cd741a562ffd6916b2ac
iran <iran.csousa21@gmail.com>
2026-09-04 23:06:06 -0300

    FEAT: Configuração do Swagger e FluentValidation

--- src/Backend-Reservas.API/Program.cs
+++ src/Backend-Reservas.API/Program.cs
@@ -1,6 +1,10 @@
+using Backend_Reservas.Application.DTOs.Reserva;
+using Backend_Reservas.Application.DTOs.Sala;
 using Backend_Reservas.Application.Interfaces;
 using Backend_Reservas.Application.Services;
+using Backend_Reservas.Application.Validators;
 using Backend_Reservas.Infrastructure.Repositories;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,11 +16,20 @@ builder.Services.AddSingleton<IReservaRepository, ReservaRepository>();
 builder.Services.AddScoped<ISalaService, SalaService>();
 builder.Services.AddScoped<IReservaService, ReservaService>();
 
+builder.Services.AddScoped<IValidator<CriarSalaDto>, CriarSalaValidator>();
+builder.Services.AddScoped<IValidator<AtualizarSalaDto>, AtualizarSalaValidator>();
+builder.Services.AddScoped<IValidator<CriarReservaDto>, CriarReservaValidator>();
+builder.Services.AddScoped<IValidator<AtualizarReservaDto>, AtualizarReservaValidator>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
